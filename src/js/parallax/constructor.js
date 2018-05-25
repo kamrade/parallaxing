2c01255e596bac80f0b940daa288d63035a9e463
Denis Mikhailov <denis.michailov@gmail.com>
2018-05-25 12:28:51 +1000

    Implement saving slide position when resize. Detection current slide when scrolling < 768

--- src/js/parallax/constructor.js
+++ src/js/parallax/constructor.js
@@ -36,6 +36,9 @@ module.exports = function ParallaxConstructor() {
 
     let headerHeight = $(".header").height();
 
+    this.$headerMenuItem.removeClass('active');
+    this.$headerMenuItem.eq(this.currentSlide-1).addClass('active');
+
     if (window.innerWidth > 768 ) {
 
       this.stepWidth = this.windowWidth;
@@ -45,12 +48,13 @@ module.exports = function ParallaxConstructor() {
       this.$slidesContainer.width(`${this.slidesWidth}px`);
       this.$main.height(this.windowHeight);
       this.$slides.height(this.windowHeight - headerHeight);
-      // this.$slides.css('min-height', this.windowHeight);
       this.$slides.width(this.windowWidth);
-      window.onwheel = this.onWheelHandler.bind(this);
-      window.onkeydown = this.globalKeyDownHandler.bind(this);
       this.currentOffset = -1 * (this.currentSlide - 1) * this.stepWidth;
       this.$slidesContainer.css('transform', `matrix(1,0,0,1,${this.currentOffset},0)`);
+
+      window.onwheel = this.onWheelHandler.bind(this);
+      window.onkeydown = this.globalKeyDownHandler.bind(this);
+      $window.scroll(null);
 
     } else {
 
@@ -58,11 +62,46 @@ module.exports = function ParallaxConstructor() {
       this.$slidesContainer.css('display', 'block');
       this.$main.height('auto');
       this.$slides.height('auto');
-      // this.$slides.css('min-height', this.windowHeight);
+      this.$slides.css('min-height', $window.height());
       this.$slides.width('auto');
-      window.onwheel = null;
-      window.onkeydown = null;
       this.$slidesContainer.css('transform', 'matrix(1,0,0,1,0,0)');
+
+      $('html, body').animate({
+        scrollTop: self.$slides.eq(this.currentSlide - 1).offset().top
+      }, 100);
+
+      window.onwheel = null;
+
+      $window.scroll(() => {
+
+        if ($window.width() < 768) {
+
+          let winScrollPos = $window.scrollTop()
+          let headerSize = 44;
+          let sensitivity = $window.height()/4 + headerSize;
+          let currentSlide = 0;
+
+          for (let i = 0, l = self.$slides.length; i < l; i++ ) {
+
+            let currOffset = winScrollPos+sensitivity;
+            let currSlideOffset = self.$slides.eq(i).offset().top;
+            let nextSlideOffset = self.$slides.eq(i+1).length ? self.$slides.eq(i+1).offset().top : 1000000;
+
+            if (currOffset > currSlideOffset && currOffset < nextSlideOffset) {
+              currentSlide = i;
+              self.currentSlide = currentSlide + 1;
+              break;
+            }
+          }
+
+          self.$headerMenuItem.removeClass('active');
+          self.$headerMenuItem.eq(self.currentSlide - 1).addClass('active');
+
+        }
+
+      });
+
+      window.onkeydown = null;
 
     }
   }
